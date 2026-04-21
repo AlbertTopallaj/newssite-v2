2d61e225f3d619ffe91b6aa6bd8d85322f8e9e19
Professor <altopallaj21@gmail.com>
2026-04-21 16:45:03 +0200

    feat: add login logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+
+function handleLogin(e) {
+e.preventDefault();
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+if (username === "admin" && password === "hemligt123") {
+    localStorage.setItem("isLoggedIn", true);
+    window.location.href = "admin.html";
+} else {
+    document.getElementById("error").classList.remove("hidden");
+}
+}
