ba0031f265baafdbf1d1b1446335558b77bc82d4
Professor <altopallaj21@gmail.com>
2026-05-02 20:44:24 +0200

    feat: add date to articles with JavaScript

--- src/display-article.js
+++ src/display-article.js
@@ -1,7 +1,8 @@
+
 const articles = JSON.parse(localStorage.getItem('articles') || '[]')
 const container = document.getElementById('articles')
 
-if(articles.length === 0){
+if (articles.length === 0) {
     const msg = document.createElement('h1')
     msg.textContent = 'No articles could be found'
     container.append(msg)
@@ -9,7 +10,7 @@ if(articles.length === 0){
     articles.forEach((article) => {
         const div = document.createElement('div')
         div.className = 'w-full p-2 shadow-2xl my-4 hover:bg-gray-500 cursor-pointer'
-        
+
         const title = document.createElement('h1')
         title.className = 'text-4xl m-5 p-10 text-wrap'
         title.textContent = article.title
@@ -18,8 +19,13 @@ if(articles.length === 0){
         content.className = 'm-5'
         content.textContent = article.content
 
+        const date = document.createElement('span')
+        date.className = 'm-5'
+        date.textContent = new Date(article.createdAt).toLocaleDateString('sv-SE')
+
         div.append(title)
         div.append(content)
+        div.append(date)
         container.appendChild(div)
     })
 }
