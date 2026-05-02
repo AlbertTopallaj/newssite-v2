a8fd3931f9e3b0e24e24cc5c3bfc2d0eb6a98bad
Professor <altopallaj21@gmail.com>
2026-05-02 21:11:49 +0200

    feat: add delete article and renderArticle function in JavaScript

--- src/display-article.js
+++ src/display-article.js
@@ -2,6 +2,10 @@
 const articles = JSON.parse(localStorage.getItem('articles') || '[]')
 const container = document.getElementById('articles')
 
+function renderArticles() {
+    const articles = JSON.parse(localStorage.getItem('articles') || '[]')
+    container.textContent = ''
+
 if (articles.length === 0) {
     const msg = document.createElement('h1')
     msg.textContent = 'No articles could be found'
@@ -17,15 +21,33 @@ if (articles.length === 0) {
 
         const content = document.createElement('p')
         content.className = 'm-5'
-        content.textContent = article.content
+        content.textContent = article.content.length > 60
+        ? article.content.slice(0, 60) + '...'
+        : article.content
 
         const date = document.createElement('span')
         date.className = 'm-5'
-        date.textContent = new Date(article.createdAt).toLocaleDateString('sv-SE')
+        date.textContent = 'Created: ' + new Date(article.createdAt).toLocaleDateString('sv-SE')
+
+        const deleteBtn = document.createElement('button')
+        deleteBtn.textContent = 'Delete'
+        deleteBtn.className = 'ml-5 mt-3 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-500'
+        deleteBtn.addEventListener('click', () =>{
+            const all = JSON.parse(localStorage.getItem('articles') || '[]')
+            const updated = all.filter(a => a.id !== article.id)
+            localStorage.setItem('articles', JSON.stringify(updated))
+            showToast('Article successfully deleted', 'error')
+            renderArticles()
+        })
 
         div.append(title)
         div.append(content)
+        div.append(deleteBtn)
         div.append(date)
         container.appendChild(div)
     })
+  }
+ 
 }
+
+ renderArticles()
