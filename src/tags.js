020a803d174c600cfbdabc014d020c44dc8a9b46
Professor <altopallaj21@gmail.com>
2026-05-17 15:16:11 +0200

    fix: change color to visual

--- src/tags.js
+++ src/tags.js
@@ -1,5 +1,5 @@
 const TAGS = {
-    java: 'bg-blue-100 text-blue-100',
+    java: 'bg-blue-100 text-blue-800',
     springboot: 'bg-green-100 text-green-800',
     default: 'bg-gray-100 text-gray-600'
 }
