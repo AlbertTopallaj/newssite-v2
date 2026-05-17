179ae2f0b70409dc4f60c3ee9ec3d9d860b192d2
Professor <altopallaj21@gmail.com>
2026-05-17 15:12:20 +0200

    refactor: add tags to tags.js

---
+++ src/tags.js
@@ -0,0 +1,10 @@
+const TAGS = {
+    java: 'bg-blue-100 text-blue-100',
+    springboot: 'bg-green-100 text-green-800',
+    default: 'bg-gray-100 text-gray-600'
+}
+
+function getTagClass(tag = '') {
+    const key = tag.toLowerCase().replace(/\s+/g, '')
+    return TAGS[key] || TAGS.default 
+}
