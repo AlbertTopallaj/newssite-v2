de58f8ae3bd123b371bac366be1861823afe14f1
Professor <altopallaj21@gmail.com>
2026-05-17 02:09:52 +0200

    feat: add toast for empty comment

--- src/commentsection.js
+++ src/commentsection.js
@@ -1,18 +1,26 @@
 const commentSection = document.getElementById('comment-section')
-const comment =  document.getElementById('comment');
+const comment = document.getElementById('comment');
 const sendComment = document.getElementById('send-comment');
 
-article.comments.forEach(function(savedComment){
+article.comments.forEach(function (savedComment) {
     const p = document.createElement('p');
 
     p.textContent = savedComment;
+
+    p.className = 'text-sm text-gray-700 border-b border-gray-100 pb-2'
 
     commentSection.appendChild(p);
 });
 
 
-function publishComment(){
+function publishComment() {
     const text = comment.value;
+
+    if (!text) {
+        showToast('The comment is empty', 'error')
+        return
+    }
+    
 
     article.comments.push(text);
 
@@ -20,9 +28,12 @@ function publishComment(){
 
     const p = document.createElement('p');
 
+    p.className = 'text-sm text-gray-700 border-b border-gray-100 pb-2'
+
     p.textContent = text;
 
     commentSection.appendChild(p);
+    comment.value = ''
 }
 
 sendComment.addEventListener('click', publishComment);
