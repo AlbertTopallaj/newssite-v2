87ddef79dce87b1b22ce254da3433f3c008689c3
Professor <altopallaj21@gmail.com>
2026-05-17 15:12:49 +0200

    refactor: remove tags from render-article.js

--- src/render-article.js
+++ src/render-article.js
@@ -2,18 +2,6 @@ console.log("renderArticle.js loaded")
 
 const container = document.getElementById('articles')
 
-const TAGS = {
-    java: 'bg-blue-100 text-blue-800',
-    springboot: 'bg-green-100 text-green-800',
-    default: 'bg-gray-100 text-gray-600'
-}
-
-function getTagClass(tag = '') {
-    const key = tag.toLowerCase().replace(/\s+/g, '')
-    return TAGS[key] || TAGS.default
-
-}
-
 function renderArticles() {
     const articles = JSON.parse(localStorage.getItem('articles') || '[]')
 
